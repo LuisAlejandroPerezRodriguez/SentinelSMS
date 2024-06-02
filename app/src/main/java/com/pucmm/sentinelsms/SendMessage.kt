3845424e8df595959f1eba4f214afc301ed68a7c
Luis Alejandro Perez Rodriguez <luisalejandro.p.r@outlook.com>
2024-06-02 17:42:37 -0400

    Basic design of the Sending message fun & basic logic for MainActivity.

--- app/src/main/java/com/pucmm/sentinelsms/SendMessage.kt
+++ app/src/main/java/com/pucmm/sentinelsms/SendMessage.kt
@@ -1,30 +1,20 @@
 package com.pucmm.sentinelsms
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SendMessage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_send_message)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        //All code here
-        val button = findViewById<AppCompatButton>(R.id.send_sms)
-        button.setOnClickListener {
-            Log.i("boton", "Button clicked")
-            val intent = Intent(this, SendMessage::class.java)
-            startActivity(intent) }
     }
 }
