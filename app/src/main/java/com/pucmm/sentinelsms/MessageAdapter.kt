7741b5652b5000a5f68ee23a64a148dd61711e2e
Luis Alejandro Perez Rodriguez <luisalejandro.p.r@outlook.com>
2024-06-10 00:22:53 -0400

    Creation of a custom MessageAdapter class for the RecyclerView.

---
+++ app/src/main/java/com/pucmm/sentinelsms/MessageAdapter.kt
@@ -0,0 +1,27 @@
+package com.pucmm.sentinelsms
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter(private val smsList: List<String>) :
+    RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+
+    class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val messageTextView: TextView = itemView.findViewById(R.id.textViewMessage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_message, parent, false)
+        return MessageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        holder.messageTextView.text = smsList[position]
+    }
+
+    override fun getItemCount() = smsList.size
+}
